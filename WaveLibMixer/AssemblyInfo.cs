1f44245acc168b83c2d6b53c836387f74d7b2694
AY <a@qx.zone>
2017-03-21 10:22:35 +0200

    Remove private and unused stuff

--- WaveLibMixer/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("WaveLibMixer")]
-
-[assembly: ComVisibleAttribute(false)]
-[assembly: GuidAttribute("D4E60049-622A-4bfa-B808-FA10313706A6")]
