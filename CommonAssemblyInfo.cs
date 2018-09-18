1fe43d7395c9baa2568624d39fd5f0da7679bcb0
AY <2341158+alexrster@users.noreply.github.com>
2018-09-18 18:28:36 +0300

    Update CommonAssemblyInfo.cs (#6)
    * Update CommonAssemblyInfo.cs
    
    * Update CommonAssemblyInfo.cs

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("ContactPoint")]
 [assembly: AssemblyProduct("ContactPoint® IP Phone")]
-[assembly: AssemblyCopyright("ContactPoint © Alex Yevtushenko")]
+[assembly: AssemblyCopyright("ContactPoint")]
 [assembly: AssemblyTrademark("IP Phone")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.9.185.0")]
-[assembly: AssemblyInformationalVersion("1.9.185.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
