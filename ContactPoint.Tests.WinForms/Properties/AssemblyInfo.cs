ff1a655ba5ebe18b641af8c489d54138085271bd
AY <a@qx.zone>
2020-05-15 11:55:22 +0300

    Upgrade project files Fix CI script

--- ContactPoint.Tests.WinForms/Properties/AssemblyInfo.cs
+++ ContactPoint.Tests.WinForms/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ContactPoint.Tests.WinForms")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ContactPoint.Tests.WinForms")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("e867edfc-3f38-48b0-8ab7-d894c743c1f7")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
