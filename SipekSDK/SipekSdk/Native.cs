b41910c1b2097fd7d1592926ab96a7bba9ec3047
AY <a@qx.zone>
2020-05-26 23:16:21 +0300

    Try full path binding

--- SipekSDK/SipekSdk/Native.cs
+++ SipekSDK/SipekSdk/Native.cs
@@ -2,6 +2,6 @@ namespace Sipek
 {
     public static class Native
     {
-        public const string PJSIP_DLL = "pjsipDll";
+        public const string PJSIP_DLL = @"D:\a\core\core\Binaries\pjsipDll.dll";
     }
 }
